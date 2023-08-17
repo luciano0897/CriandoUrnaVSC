722230a81ade94ee31ee2c43639a44afdbcbc4cf
Luciano Batista <lucianobatista0897@gmail.com>
2023-08-17 12:39:05 -0300

    js em andamento

---
+++ urna/etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 30301,
+                nome: 'Caique Batista',
+                partido: 'VQT',
+                fotos:[
+                    {url: 'adultoHomem.png', legenda: 'vereador'}
+                ]
+
+            },
+            {
+                numero: 30321,
+                nome: 'kelvin Batista',
+                partido: 'LGBT',
+                fotos:[
+                    {url: 'jovemHomem.png', legenda: 'vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 15,
+                nome: 'Luciano',
+                partido: 'ABC',
+                vice: 'Rei',
+                fotos:[
+                    {url: 'fotoluciano01.jpg', legenda: 'Prefeito'},
+                    {url: 'velhaMulher.png', legenda: 'Vice-Prefeita', small:true}
+                ]
+
+            },
+            {
+                numero: 33,
+                nome: 'Jairo',
+                partido: 'BAP',
+                vice: 'Zup',
+                fotos:[
+                    {url: 'velhoHomem.png', legenda: 'Prefeito'},
+                    {url: 'velhaHomem.png', legenda: 'Vice-Prefeito', small:true}
+                ] 
+            }
+        ]
+
+    }
+    
+]
---
+++ urna/script.js
@@ -0,0 +1,63 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector ('.d-1-2 span');
+let descrição = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-esquerda');
+let numeros = document.querySelector('.d-1-3');
+
+// variaveis de controle de ambiente 
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapas(){
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++){
+        if (i === 0){
+            numeroHtml += '<div class="numero pisca"></div>';
+        }else{ 
+        numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descrição.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+function atualiaInterface(){
+    alert("finalizou de digita o voto!");
+
+}
+// esta função buscar o espaço que esta piscando para adionar um numero 
+// este nextElementSibling puxar o elemento que esta do lado 
+function clicou(n){
+    let elNumero = document.querySelector('.numero.pisca');
+    if (elNumero !== null){
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null)
+        elNumero.nextElementSibling.classList.add('pisca');  
+    }else{
+        atualiaInterface();
+
+    }
+   
+}
+function branco(){
+    alert("Clicou branco");
+}
+function corrige(){
+    alert("Clicou em corrige");
+}
+function confirma(){
+    alert("Clicou em confirma");
+}
+
+comecarEtapas();
